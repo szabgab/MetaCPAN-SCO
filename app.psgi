94db29297470e00357f347bcd0deb54717393454
Gabor Szabo <gabor@szabgab.com>
2014-10-20 17:00:39 +0300

    add app.psgi and some basic code to show a main page and to give 404 otherwise

---
+++ app.psgi
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use lib 'lib';
+use MetaCPAN::SCO;
+
+my $app = MetaCPAN::SCO->run;
--- lib/MetaCPAN/SCO.pm
+++ lib/MetaCPAN/SCO.pm
@@ -2,6 +2,8 @@ package MetaCPAN::SCO;
 use strict;
 use warnings;
 
+use Plack::Request;
+
 our $VERSION = '0.01';
 
 =head1 NAME
@@ -10,6 +12,18 @@ SCO - search.cpan.org clone
 
 =cut
 
+sub run {
+	my $app = sub {
+		my $env = shift;
+
+		my $request = Plack::Request->new($env);
+		if ($request->path_info eq '/') {
+			return [ '200', [ 'Content-Type' => 'text/plain' ], ['Hello'], ];
+		}
+
+		return [ '404', [ 'Content-Type' => 'text/html' ], ['404 Not Found'], ];
+	};
+}
 
 
 1;
