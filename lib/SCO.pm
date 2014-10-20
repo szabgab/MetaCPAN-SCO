4ca4244f6e51aa8ac0129ee72ad1000e79b105aa
Gabor Szabo <gabor@szabgab.com>
2014-10-20 16:20:03 +0300

    Skeleton of a badly named module. Skeleton test

---
+++ lib/SCO.pm
@@ -0,0 +1,16 @@
+package SCO;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+SCO - search.cpan.org clone
+
+=cut
+
+
+
+1;
+
---
+++ t/10-sco.t
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+use Test::More;
+use Test::WWW::Mechanize;
+
+plan tests => 1;
+
+
+pass;
